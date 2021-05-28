4b1f47dc559a5cd9be6df50021f5b378f65fcf6a
Eduard <trankvilizator008@gmail.com>
2021-05-29 01:51:33 +0300

    Theme

---
+++ Assets/Scripts/Enums/ThemeType.cs
@@ -0,0 +1,5 @@
+﻿public enum ThemeType
+{
+    Dark,
+    Light
+}
--- Assets/Scripts/ScriptableObjects/Theme.cs
+++ Assets/Scripts/ScriptableObjects/Theme.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Theme : MonoBehaviour
+public class Theme : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
